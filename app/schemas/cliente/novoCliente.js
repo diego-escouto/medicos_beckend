8f5346c147a92add0397656ddd5ae8ec1b1c7be9
Diego Escouto <diegonzales72@gmail.com>
2025-12-15 09:12:00 -0300

    update novo cliente
    atualizando a quantidade mínima de caracteres na senha e nome

--- app/schemas/cliente/novoCliente.js
+++ app/schemas/cliente/novoCliente.js
@@ -1,9 +1,9 @@
 module.exports = {
   type: 'object',
   properties: {
-    nome: { type: 'string', minLength: 5 },
+    nome: { type: 'string', minLength: 4 },
     email: { type: 'string' },
-    senha: { type: 'string' },
+    senha: { type: 'string', minLength: 8},
   },
   required: ['email', 'senha'],
   additionalProperties: false,
